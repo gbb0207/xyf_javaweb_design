954ecbb05e10d3044c0219e060568282e2714b49
wyb <2429468825@qq.com>
2023-10-10 23:50:06 +0800

    提交消息

---
+++ README.md
@@ -0,0 +1,2 @@
+# StuInfoSystem
+学生信息管理系统
---
+++ src/main/java/com/lc/demo/bean/Admin.java
@@ -0,0 +1,14 @@
+package com.lc.demo.bean;
+
+
+import lombok.Data;
+
+@Data
+public class Admin {
+
+    private String adminId;
+
+    private String adminName;
+
+    private String adminPass;
+}
